84a51f7b0186c25470cbadbfb1a318df27f33fe8
Hubert Abasto <habasto@gmil.com>
2026-09-03 23:34:45 -0400

    Verison 2

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	fmt.Println("gonet - Network Toolkit")
+
+	if len(os.Args) < 2 {
+		fmt.Println("Uso: gonet <comando> [argumentos]")
+		return
+	}
+
+	comando := os.Args[1]
+
+	fmt.Println("Comando:", comando)
+
+	if len(os.Args) >=3 {
+		destino := os.Args[2]
+		fmt.Println("Destino:", destino)
+	}
 }
