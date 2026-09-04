87f475bd886abe25629f2dea25c4c6ddd3a53cbe
Hubert Abasto <habasto@gmil.com>
2026-09-03 23:42:57 -0400

    Despachador

--- main.go
+++ main.go
@@ -15,6 +15,22 @@ func main() {
 
 	comando := os.Args[1]
 
+	switch comando {
+
+	case "ping":
+		fmt.Println("Ejecutando ping")
+
+	case "dns":
+                fmt.Println("Ejecutando consulta DNS")
+
+	case "tcp":
+                fmt.Println("Ejecutando prueba TCP")
+
+	default:
+		fmt.Println("Comndo desconocido:", comando)
+	}
+
+
 	fmt.Println("Comando:", comando)
 
 	if len(os.Args) >=3 {
